302262ca4f3ccbfe878085e1a41a02e17d0b01e3
AlejandroValenciaG <valenciaguerraomar.94@gmail.com>
2023-05-09 08:30:08 -0500

    Routs login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import './header.css'
 
 export const Header = () => {
   return (
     <>
-    <header>
+    <header className='containerHeader'>
         <h1>Trainig project react</h1>
     </header>
     
     </>
   )
 }
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './Login'
-import './Login.css'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
